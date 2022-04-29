47838bfcb459590bcab38c9533bd8eff7dbcc724
jwyamaguchi <jwyamaguchi@ucsd.edu>
2022-04-28 21:44:28 -0700

    commit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -30,8 +30,8 @@ public class MarkdownParse {
                 break;
             }
             int space = markdown.indexOf(" ", currentIndex);
-            // openParen == closeBracket + 1 && 
-            if (exclamationPoint != openBracket - 1 && space < openParen) {
+            // && space < openParen
+            if (openParen == closeBracket + 1 && exclamationPoint != openBracket - 1 && space < openParen) {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
